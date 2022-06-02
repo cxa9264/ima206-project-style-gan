5b5a309f9d6c911e036fffaaf80bfa9459cfea00
Tianyu Li <tianyu.li0613@gmail.com>
2022-06-02 19:40:26 +0800

    update run_stylegan.sh

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 IMA206 project on style gan
 
 Based on Pytorch implemented stylegan2-ada and InterFaceGan.
+
+## StyleGan
+
+Generate cifar-10 images
+```
+sh run_stylegan.sh cifar
+```
+
+Generate MetFaces images
+```
+sh run_stylegan.sh metfaces
+```
+
+Generate mixing example
+```
+sh run_stylegan.sh mixing
+```
---
+++ run_stylegan.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# python generate.py --outdir=out --trunc=0.7 --seeds=600-605 \
+#     --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
+
+case $1 in
+    cifar)
+        echo "running with cifar-10"
+        cd stylegan2-ada-pytorch
+        python generate.py --outdir=out --seeds=0-35 --class=1 \
+            --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/cifar10.pkl;;
+
+    metfaces)
+        echo "running with MetFaces"
+        cd stylegan2-ada-pytorch
+        python generate.py --outdir=out --trunc=1 --seeds=85,265,297,849 \
+            --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl;;
+    mixing)
+        echo "runing with mixing"
+        cd stylegan2-ada-pytorch
+        python style_mixing.py --outdir=out --rows=85,100,75,458,1500 --cols=55,821,1789,293 \
+             --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl;;
+    ?)
+        echo "unkown argument";;
+esac
