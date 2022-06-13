c985d35cd720747e97e5136c1232d5b58ef38292
Tianyu Li <tianyu.li0613@gmail.com>
2022-06-14 00:46:36 +0800

    update move

---
+++ run_move.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd interfacegan
+
+rm -r ../result
+
+python move_a_step.py \
+    --save_path ../result \
+    --boundary /home/litianyu/ima206-project-style-gan/interfacegan/boundaries_ori/stylegan_celebahq_gender_boundary.npy
