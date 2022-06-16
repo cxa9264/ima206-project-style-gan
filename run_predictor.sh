4a5e574c9ed8290faee8e8192479eea153f54408
Tianyu Li <tianyu.li0613@gmail.com>
2022-06-16 08:46:55 +0800

    100k data

--- run_predictor.sh
+++ run_predictor.sh
@@ -7,7 +7,7 @@ python predict_gender.py \
     --img_path /home/litianyu/ima206-project-style-gan/result \
     `# --img_path /home/litianyu/ima206-project-style-gan/interfacegan/data/stylegan_celebahq_data` \
     `# index of attribute, 20 gender, 31 smile` \
-    --target $1 4 8 15 20 31  \
+    --target 4 8 15 20 31  \
     --move_target $1 \
     `# save_path, plese make sure the directory exists` \
     --save_path ../move_result \
